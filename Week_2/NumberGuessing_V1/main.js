0eaa023705e13ef30e2bcdd49e763c4def840a3f
LamBrandon <bl66186@student.musd.org>
2018-06-27 14:35:47 -0700

    Updated Data Validation

--- Week_2/NumberGuessing_V1/main.js
+++ Week_2/NumberGuessing_V1/main.js
@@ -5,13 +5,13 @@ function playGame(){
     //get the low and high bounds
     //uses parseInt() to make sure we have numbers and not strings
     var from = parseInt(prompt("Enter the lower bound.")); 
-while(from<0||from>1000||isNaN(from)){
-        from = parseInt(prompt("Enter a number greater than 0."));
-}
+    while(from<0||from>1000||isNaN(from)){
+        from = parseInt(prompt("Enter a number between 0 and 1000."));
+    }
     var to = parseInt(prompt("Enter the higher bound.")); 
-while(to<from||to>1000||isNaN(to)){
-        to = parseInt(prompt("Enter a number less than 1000."));
-}
+    while(to<from||to>1000||isNaN(to)){
+        to = parseInt(prompt("Enter a number between 0 than 1000."));
+    }
     //get an integer between [from, to]
     //Math.random() returns decimals, used Math.round to get whole number
     var target = Math.round(Math.random() * (to - from) + from);
@@ -23,13 +23,13 @@ while(to<from||to>1000||isNaN(to)){
     //loop until user guesses correct number
     while(currentGuess != target){
         if(currentGuess > from || currentGuess < to || isNaN(currentGuess)){
-            currentGuess = parseInt(prompt("Enter gueses between from and to."))
+            currentGuess = parseInt(prompt("You did not enter a number between from and to."))
         }
         if(currentGuess < target){
-        currentGuess = parseInt(prompt("Enter a higher number"));
+            currentGuess = parseInt(prompt("Enter a higher number"));
 
         totalGuesses++;
-        }else if (currentGuess > target){
+        }else if (currentGuess > target){""
             currentGuess = parseInt(prompt("Enter a lower number"));
         
             totalGuesses ++;
