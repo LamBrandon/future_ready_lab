f80a2b0c4682ade33ac2c1c42834281a8a4bef5d
LamBrandon <bl66186@student.musd.org>
2018-07-06 15:38:50 -0700

    Random Bouncing

--- Week_3/Processing JS/sketch.js
+++ Week_3/Processing JS/sketch.js
@@ -1,4 +1,5 @@
 //define circle object
+/*
 var circle = {
     diameter:80,
     xCoor: 0,
@@ -7,33 +8,47 @@ var circle = {
     xSpeed: 15,
     ySpeed: 15
 }
+*/
+function Circle(x, y, size, color, xSpeed, ySpeed){
+    this.xCoor = x;
+    this.yCoor = y;
+    this.diameter = size;
+    this.color = color;
+    this.xSpeed = xSpeed;
+    this.ySpeed = ySpeed;
+}
+
+var circle = new Circle(0,0,80, [255,0,0], 2, 10);
+var circle2 = new Circle(0, 30, 50, 7, 7);
+var circleList = [circle, circle2, new Circle(100,0,18)]
 
 function setup() {
     createCanvas(640, 480);
     background(20, 255, 460);
     frameRate(15);
 }
+function ballAmount() {
+    var random = random([2,5,9,16]);
+    return ballAmount[random];
+}
+
 
 function draw() {
     //creates drawing for
+    background(20, 255, 460)
     fill(circle.color);
     ellipse(circle.xCoor, circle.yCoor, circle.diameter);
 
-    
+
     //if we hit the right edge of the canvas
-    if(circle.xCoor >640){
-        circle.color = [Math.random()*255,Math.random()*255,Math.random()*255]//makes circle random color
+    if(circle.xCoor >640 || circle.xCoor <0){
+        circle.color = [random(0, 255),random(0, 255),random(0, 255)]//makes circle random color
         circle.xSpeed = -circle.xSpeed; //reverse direction
-    }else if(circle.xCoor <0){
-        circle.color = [Math.random()*255,Math.random()*255,Math.random()*255]; //make the circle random color
-        circle.xSpeed = -circle.xSpeed; //changes direction
-    }else if(circle.yCoor <0){
-        circle.diameter = Math.random()*100; //set range for diamater of circle
+
+    }else if(circle.yCoor <0 || circle.yCoor >480){
+        circle.diameter = random(0, 100); //set range for diamater of circle
         circle.ySpeed = -circle.ySpeed; //changes direction
-    }else if(circle.yCoor >480){
-        circle.diameter =Math.random()*100; //set range for diameter of circle
-        circle.ySpeed = -circle.ySpeed; //changes directiom
-    
+
         
     }
     //change position of circle
