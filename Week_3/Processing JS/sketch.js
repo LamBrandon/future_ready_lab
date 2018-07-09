2ba3a8487679bb51b5ef1c78e12887b0a1615e3d
LamBrandon <bl66186@student.musd.org>
2018-07-09 10:59:40 -0700

    Random Bouncing

--- Week_3/Processing JS/sketch.js
+++ Week_3/Processing JS/sketch.js
@@ -1,57 +1,75 @@
-//define circle object
-/*
-var circle = {
-    diameter:80,
+//define a circle object
+/*var circle = {
+    diameter: 80,
     xCoor: 0,
-    yCoor:0,
-    color:[255,0,0], //RGB values give circle a red color
-    xSpeed: 15,
-    ySpeed: 15
+    yCoor: 0,
+    color: [255,0,0], //RGB values gives circle a red color
+    xSpeed: 5,
+    ySpeed: 5
 }
 */
+
+//ellipse(x, y, w,)
+//ellipse(x, y, w, h)
 function Circle(x, y, size, color, xSpeed, ySpeed){
     this.xCoor = x;
     this.yCoor = y;
     this.diameter = size;
-    this.color = color;
+    this.color = color; 
     this.xSpeed = xSpeed;
     this.ySpeed = ySpeed;
 }
 
 var circle = new Circle(0,0,80, [255,0,0], 2, 10);
 var circle2 = new Circle(0, 30, 50, 7, 7);
-var circleList = [circle, circle2, new Circle(100,0,18)]
+
 
 function setup() {
-    createCanvas(640, 480);
-    background(20, 255, 460);
-    frameRate(15);
-}
-function ballAmount() {
-    var random = random([2,5,9,16]);
-    return ballAmount[random];
+    createCanvas(parseInt(prompt("Enter canvas width")),parseInt(prompt("Enter canvas height"))); //Canvas size
+    frameRate(50);
+    ballAmount();
 }
 
 
-function draw() {
-    //creates drawing for
-    background(20, 255, 460)
-    fill(circle.color);
-    ellipse(circle.xCoor, circle.yCoor, circle.diameter);
+function randomColor(){
+    return[random(0, 255), random(0, 255), random(0, 255)];
+}
 
 
-    //if we hit the right edge of the canvas
-    if(circle.xCoor >640 || circle.xCoor <0){
-        circle.color = [random(0, 255),random(0, 255),random(0, 255)]//makes circle random color
-        circle.xSpeed = -circle.xSpeed; //reverse direction
+//Random number & loops until circles equals a number within number array
+function ballAmount(){
+    var circles = random([2,5,9,16]);
+    for(var numBall = 0; numBall < circles; numBall ++){
+        circleList.push(new Circle(0,0, 80, [random(0, 255), random(0, 255), random(0, 255)], random(0, 20), random(0, 20))); //adds new circles to empty array
+    };
+}
 
-    }else if(circle.yCoor <0 || circle.yCoor >480){
-        circle.diameter = random(0, 100); //set range for diamater of circle
-        circle.ySpeed = -circle.ySpeed; //changes direction
+//empty array
+var circleList = [];
 
+
+function draw(){
+    background(255,255,255); //White Background
+
+    
+    for(var i = 0; i < circleList.length; i++){
+        fill(circleList[i].color);
+        ellipse(circleList[i].xCoor, circleList[i].yCoor, circleList[i].diameter);
         
+        //Hit the right and left side of canvas
+        if(circleList[i].xCoor > width || circleList[i].xCoor <0){
+            circleList[i].color = [random(0,255), random(0,255), random(0,255)] //Changes to random Color
+            circleList[i].xSpeed = -circleList[i].xSpeed; //changes direction
+
+
+        //Hit the right and left side of canvas
+        }if(circleList[i].yCoor < 0 || circleList[i].yCoor > height){
+            circleList[i].diameter = random(0,100); //changes to random diameter 
+            circleList[i].ySpeed = -circleList[i].ySpeed; //changes direction
+        }
+            
+        //changes position of circle
+        circleList[i].xCoor += circleList[i].xSpeed;
+        circleList[i].yCoor += circleList[i].ySpeed;
     }
-    //change position of circle
-    circle.xCoor += circle.xSpeed; 
-    circle.yCoor += circle.ySpeed;
 }
